94da27b811d833b5c60adf88698db6c00f0235a4
Prince Kothari <197196645+princekotharii@users.noreply.github.com>
2026-02-23 12:46:25 +0530

    refactor: improve layout structure and styling in TutorSidebar and TutorLayout components

--- client/main-app/src/components/tutor/TutorLayout.js
+++ client/main-app/src/components/tutor/TutorLayout.js
@@ -3,16 +3,18 @@ import MobileTabBar from './MobileTabBar';
 
 export default function TutorLayout({ children }) {
   return (
-    <div className="flex min-h-screen bg-[#0F0F1E]">
+    <div className="min-h-screen bg-[#0F0F1E] flex flex-col lg:flex-row">
       {/* Desktop Sidebar - Only visible on large screens */}
-      <div className="hidden lg:block sticky top-0 left-0 h-screen">
+      <div className="hidden lg:block w-64 lg:w-72 shrink-0 border-r border-gray-800/50">
         <TutorSidebar />
       </div>
 
-      {/* Main Content Area - Responsive padding and margins */}
-      <main className="flex-1 w-full p-3 sm:p-4 lg:p-5 pb-16 lg:pb-5">
-        <div className="max-w-7xl mx-auto">
-          {children}
+      {/* Main Content Area */}
+      <main className="flex-1 w-full overflow-hidden flex flex-col">
+        <div className="flex-1 overflow-y-auto p-3 sm:p-4 lg:p-6 pb-16 lg:pb-6">
+          <div className="max-w-7xl mx-auto">
+            {children}
+          </div>
         </div>
       </main>
 
